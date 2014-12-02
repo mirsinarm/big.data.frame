2b24c00a91ab82fc4ae2a4ae99d6c7ce2e4650cf
Baobao Zhang <13bzhang@gmail.com>
2014-12-02 17:02:30 -0500

    add testing

---
+++ testing/bigreadtable_test.R
@@ -0,0 +1,24 @@
+###########################
+# big.read.table tests
+
+setwd("~/Dropbox/stat662/big.data.frame/testing")
+# WITH HEADER
+wh <- data.frame(rep(c(1L:26L),2000),rep(letters,2000),rep(rnorm(26),2000))
+names(wh) <- c("int","factor","num")
+write.csv(wh,"withheader.csv",row.names=F)
+rwh <- read.csv("withheader.csv",header = T)
+file <- "withheader.csv"
+
+
+rwh1 <- big.read.table(file = "withheader.csv",as.is = T,header = TRUE)
+rwh2 <- big.read.table(file = "withheader.csv",as.is = T,header = FALSE)
+
+
+length(read.table("withheader.csv",sep=",",nrows=1,header=F))
+
+# WITHOUT HEADER
+wh <- data.frame(rep(c(1L:26L),2000),rep(letters,2000),rep(rnorm(26),2000))
+#names(wh) <- c("int","factor","num")
+write.table(wh,"withoutheader.csv",sep=",",row.names=FALSE,col.names=FALSE)
+wwh <- read.csv("withoutheader.csv",header = F,sep = ",")
+bwh <- big.read.table(file = "withoutheader.csv",sep=",",header=F)
