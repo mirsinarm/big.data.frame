44f4ea4af1934d3da7a68cdf647640892a70ff36
Miranda Sinnott-Armstrong <mirsinarm@users.noreply.github.com>
2014-12-14 12:20:37 -0500

    some stuff in testthat

--- tests/testthat/testCreate.R
+++ tests/testthat/testCreate.R
@@ -72,7 +72,7 @@ test_that("Extractions 1", {
   y <- data.frame(first=rep(-1.23, 10), second=rep("A", 10),
                   stringsAsFactors=FALSE)
   expect_that(x[], equals(y))
-  expect_that(x[1,], equals(y[1,]))
+  expect_that(x[1,], equals(y[1,])) # this fails ... no idea why ... print, class, typeof all look identical
   expect_that(x[1:2,], equals(y[1:2,]))
   expect_that(x[-c(1:2),], equals(y[-c(1:2),])) # removes the last two rows instead of the first two rows
   expect_that(x[1,1], equals(y[1,1]))
